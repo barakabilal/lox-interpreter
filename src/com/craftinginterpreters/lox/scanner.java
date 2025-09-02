3e214f79af09e0e998733311bb166158a5f76ca9
hitiuees <ghbadr132800@gmail.com>
2025-09-02 22:08:22 +0100

    add tokens recoganization pattern and function that read the source

--- src/com/craftinginterpreters/lox/scanner.java
+++ src/com/craftinginterpreters/lox/scanner.java
@@ -10,6 +10,9 @@ import static com.craftinginterpreters.lox.tokentype.*;
 class scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     scanner(String source) {
         this.source = source;
@@ -21,8 +24,40 @@ class scanner {
             scanToken();
         }
 
+
         tokens.add(new Token(EOF, "", null, line));
+
         return tokens;
+    }
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+        }
+    }
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(tokentype type) {
+        addToken(type, null);
+    }
+
+    private void addToken(tokentype type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
 
